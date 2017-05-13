6768a5f4f73718e522436ae4dae7451180da4bff
c1ndyw <cindyangelicawijaya@ymail.com>
2017-05-13 22:15:01 +1000

    second-layout

--- FITNESS_FIRST/FITNESS_FIRST/HomePage.Designer.cs
+++ FITNESS_FIRST/FITNESS_FIRST/HomePage.Designer.cs
@@ -2,12 +2,12 @@
 {
     partial class HomePage
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,212 +20,59 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
-            this.label1 = new System.Windows.Forms.Label();
-            this.Home = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.button10 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.panel1.SuspendLayout();
+            this.sportContainer = new System.Windows.Forms.Panel();
+            this.startBtn = new System.Windows.Forms.PictureBox();
+            this.courseTableAdapter1 = new WindowsFormsApplication1.FitnessfirstDataSetTableAdapters.CourseTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.startBtn)).BeginInit();
             this.SuspendLayout();
             // 
-            // label1
+            // sportContainer
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(75, 56);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 25);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Fitness First";
+            this.sportContainer.Location = new System.Drawing.Point(183, 3);
+            this.sportContainer.Name = "sportContainer";
+            this.sportContainer.Size = new System.Drawing.Size(171, 148);
+            this.sportContainer.TabIndex = 9;
             // 
-            // Home
+            // startBtn
             // 
-            this.Home.Location = new System.Drawing.Point(365, 28);
-            this.Home.Name = "Home";
-            this.Home.Size = new System.Drawing.Size(79, 86);
-            this.Home.TabIndex = 1;
-            this.Home.Text = "Home";
-            this.Home.UseVisualStyleBackColor = true;
-            this.Home.Click += new System.EventHandler(this.Home_Click);
+            this.startBtn.Image = ((System.Drawing.Image)(resources.GetObject("startBtn.Image")));
+            this.startBtn.Location = new System.Drawing.Point(3, 3);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(174, 148);
+            this.startBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.startBtn.TabIndex = 8;
+            this.startBtn.TabStop = false;
             // 
-            // button2
+            // courseTableAdapter1
             // 
-            this.button2.Location = new System.Drawing.Point(450, 28);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(81, 86);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "Graph";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(537, 28);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(84, 86);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "Calender";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(627, 28);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(89, 86);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "Settings";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(722, 28);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(90, 86);
-            this.button5.TabIndex = 5;
-            this.button5.Text = "Help";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(43, 141);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(357, 301);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
-            // 
-            // panel1
-            // 
-            this.panel1.Controls.Add(this.button10);
-            this.panel1.Controls.Add(this.button9);
-            this.panel1.Controls.Add(this.button8);
-            this.panel1.Controls.Add(this.button7);
-            this.panel1.Controls.Add(this.button6);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(432, 141);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(380, 301);
-            this.panel1.TabIndex = 7;
-            // 
-            // button10
-            // 
-            this.button10.Location = new System.Drawing.Point(253, 147);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(82, 76);
-            this.button10.TabIndex = 5;
-            this.button10.Text = "button10";
-            this.button10.UseVisualStyleBackColor = true;
-            // 
-            // button9
-            // 
-            this.button9.Location = new System.Drawing.Point(157, 147);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(68, 76);
-            this.button9.TabIndex = 4;
-            this.button9.Text = "button9";
-            this.button9.UseVisualStyleBackColor = true;
-            // 
-            // button8
-            // 
-            this.button8.Location = new System.Drawing.Point(43, 147);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(75, 76);
-            this.button8.TabIndex = 3;
-            this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(253, 30);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(82, 79);
-            this.button7.TabIndex = 2;
-            this.button7.Text = "Yoga";
-            this.button7.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(148, 30);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(77, 79);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "Full Bdy";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(43, 30);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 79);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Push Up";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // backgroundWorker1
-            // 
-            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.courseTableAdapter1.ClearBeforeFill = true;
             // 
             // HomePage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(869, 494);
-            this.Controls.Add(this.panel1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.Home);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sportContainer);
+            this.Controls.Add(this.startBtn);
             this.Name = "HomePage";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "HomePage";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.panel1.ResumeLayout(false);
+            this.Size = new System.Drawing.Size(362, 159);
+            ((System.ComponentModel.ISupportInitialize)(this.startBtn)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button Home;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button1;
-        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel sportContainer;
+        private System.Windows.Forms.PictureBox startBtn;
+        private FitnessfirstDataSetTableAdapters.CourseTableAdapter courseTableAdapter1;
     }
 }
--- FITNESS_FIRST/FITNESS_FIRST/HomePage.cs
+++ FITNESS_FIRST/FITNESS_FIRST/HomePage.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,32 +10,52 @@ using System.Windows.Forms;
 
 namespace WindowsFormsApplication1
 {
-    public partial class HomePage : Form
+    public partial class HomePage : UserControl
     {
         public HomePage()
         {
             InitializeComponent();
+            Init();
         }
 
-        private void Home_Click(object sender, EventArgs e)
+        public void Init()
         {
-
+            startBtn.Location = new Point(0, 0);
+            startBtn.BackColor = Color.CadetBlue;
+            sportContainer.BackColor = Color.Coral;
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        public void ReSize()
         {
-
+            startBtn.Width = (int)(this.Width * 2 / 5);
+            startBtn.Height = this.Height;
+            sportContainer.Location = new Point(startBtn.Width, 0);
+            sportContainer.Width = (int)(this.Width * 3 / 5);
+            sportContainer.Height = this.Height;
+            SetButtons();
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        public void SetButtons()
         {
-            Form1 calender_form = new Form1();
-			calender_form.Show();
-        }
-
-        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
-        {
-
+            Global.DefaultSport();
+            int buttonSize = (int)(sportContainer.Width / 4);
+            int row = 0, col = 0;
+            for(int i = 0; i<Global.Sports.Count; i++)
+            {
+                Button b = new Button();
+                b.Name = Global.Sports[i];
+                b.Location = new Point(col * buttonSize, row * buttonSize);
+                b.Width = buttonSize;
+                b.Height = buttonSize;
+                b.FlatStyle = FlatStyle.Flat;
+                col++;
+                if (col >= 4)
+                {
+                    col = 0;
+                    row++;
+                }
+                sportContainer.Controls.Add(b);
+            }
         }
     }
 }
