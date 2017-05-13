5fe112809c8f3e2078f6db69989b990eb2c6cbb3
101272020-Gabriel <101272020@student.swin.edu.au>
2017-05-13 22:24:17 +1000

    Add Calendar

---
+++ FITNESS_FIRST/Calendar.NET/TodayButton.cs
@@ -0,0 +1,23 @@
+﻿namespace Calendar.NET
+{
+    internal class TodayButton : CoolButton
+    {
+        public TodayButton()
+        {
+            Size = new System.Drawing.Size(72, 29);
+            ButtonText = "Today";
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // TodayButton
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.Name = "TodayButton";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
