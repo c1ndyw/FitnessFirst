b3d241835ddd0043a42cb9fae2a7b4bc5f7cda7b
101272020-Gabriel <101272020@student.swin.edu.au>
2017-05-13 22:26:07 +1000

    add calendar page

--- FITNESS_FIRST/FITNESS_FIRST/CalendarPage.cs
+++ FITNESS_FIRST/FITNESS_FIRST/CalendarPage.cs
@@ -7,22 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Calendar.NET;
 
-namespace WindowsFormsApplication1
+namespace FitnessFirst
 {
     public partial class CalendarPage : UserControl
     {
-       
+        private Calendar.NET.Calendar _calendar = new Calendar.NET.Calendar();
         public CalendarPage()
         {
             InitializeComponent();
         }
 
-      
+        public void ReSize()
+        {
+            _calendar.Location = new Point(10, 10);
+            _calendar.Width = this.Width - 20;
+            _calendar.Height = this.Height - 50;
+        }
 
         private void CalendarPage_Load(object sender, EventArgs e)
         {
-        
+            ReSize();
+            Controls.Add(_calendar);
+        }
+
+        public Calendar.NET.Calendar Calendar
+        {
+            get
+            {
+                return _calendar;
+            }
         }
     }
 }
