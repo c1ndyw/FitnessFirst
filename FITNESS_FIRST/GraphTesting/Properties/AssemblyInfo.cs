14199863230540a558909b0a03ee12c9acb0df6a
AllNiceNamesAreTaken <AllNiceNamesAreTaken@users.noreply.github.com>
2017-05-13 22:50:27 +1000

    add-graph-test

---
+++ FITNESS_FIRST/GraphTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testing")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testing")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("45af65e6-699c-4fa2-8d2c-3cb98f9364ac")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ FITNESS_FIRST/GraphTesting/TestGraph.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms.DataVisualization.Charting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FitnessFirst;
+
+namespace Testing
+{
+    [TestClass]
+    public class TestGraph
+    {
+        [TestMethod]
+        public void TestInitColor()
+        {
+            GraphPage graph = new GraphPage();
+            graph.Init();
+            Assert.AreEqual(graph.BackColor, Color.White);
+        }
+
+        [TestMethod]
+        public void TestLocation()
+        {
+            GraphPage graph = new GraphPage();
+            graph.setLocation();
+            Assert.AreEqual(graph.Chart.Location, new Point(100, 100));
+        }
+
+        [TestMethod]
+        public void TestResize()
+        {
+            GraphPage graph = new GraphPage();
+            graph.setSize();
+
+            Assert.AreEqual(graph.Chart.Size, new Size(graph.Width - 100 * 2, graph.Height - 100 * 2));
+        }
+    }
+}
