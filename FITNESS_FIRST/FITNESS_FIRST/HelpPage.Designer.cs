56e1d1278244c3e05172ac10a7b0f8d1342d5f37
101272020-Gabriel <101272020@student.swin.edu.au>
2017-05-24 15:06:41 +1000

    Add files via upload

--- FITNESS_FIRST/FITNESS_FIRST/HelpPage.Designer.cs
+++ FITNESS_FIRST/FITNESS_FIRST/HelpPage.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace WindowsFormsApplication1
+﻿namespace FitnessFirst
 {
     partial class HelpPage
     {
@@ -28,10 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.title = new System.Windows.Forms.Label();
+            this.OkBtn = new System.Windows.Forms.Button();
+            this.CancelBtn = new System.Windows.Forms.Button();
+            this.desc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Location = new System.Drawing.Point(17, 18);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(23, 13);
+            this.title.TabIndex = 0;
+            this.title.Text = "title";
+            // 
+            // OkBtn
+            // 
+            this.OkBtn.Location = new System.Drawing.Point(66, 97);
+            this.OkBtn.Name = "OkBtn";
+            this.OkBtn.Size = new System.Drawing.Size(75, 23);
+            this.OkBtn.TabIndex = 1;
+            this.OkBtn.Text = "OK";
+            this.OkBtn.UseVisualStyleBackColor = true;
+            this.OkBtn.Click += new System.EventHandler(this.OkBtn_Click);
+            // 
+            // CancelBtn
+            // 
+            this.CancelBtn.Location = new System.Drawing.Point(147, 97);
+            this.CancelBtn.Name = "CancelBtn";
+            this.CancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.CancelBtn.TabIndex = 2;
+            this.CancelBtn.Text = "Cancel";
+            this.CancelBtn.UseVisualStyleBackColor = true;
+            this.CancelBtn.Click += new System.EventHandler(this.CancelBtn_Click);
+            // 
+            // desc
+            // 
+            this.desc.AutoSize = true;
+            this.desc.Location = new System.Drawing.Point(17, 44);
+            this.desc.Name = "desc";
+            this.desc.Size = new System.Drawing.Size(30, 13);
+            this.desc.TabIndex = 3;
+            this.desc.Text = "desc";
+            // 
+            // HelpPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.desc);
+            this.Controls.Add(this.CancelBtn);
+            this.Controls.Add(this.OkBtn);
+            this.Controls.Add(this.title);
+            this.Name = "HelpPage";
+            this.Size = new System.Drawing.Size(242, 132);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button OkBtn;
+        private System.Windows.Forms.Button CancelBtn;
+        private System.Windows.Forms.Label desc;
     }
 }
